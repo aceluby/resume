cb3f89bc1af2eda380d8fac5c505a8b56c762303
Jake Luby <jake.luby@gmail.com>
2019-06-04 07:28:02 -0500

    Initial Commit

---
+++ src/components/about/about.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+export default class About extends Component {
+    render() {
+        let resumeData = this.props.resumeData;
+        return (
+            <section id="about">
+                <div className="row">
+
+                    <div className="three columns">
+
+                        <img className="profile-pic"  src="https://media.licdn.com/dms/image/C4E03AQEEKBDkpdoKwA/profile-displayphoto-shrink_200_200/0?e=1565222400&v=beta&t=K1hj-UcXtyYRN6ahApVNZwHqT_MD19micnakHM55aLc" alt="" />
+
+                    </div>
+
+                    <div className="nine columns main-col">
+
+                        <h2>About Me</h2>
+                        <p>
+                            {
+                                resumeData.aboutme
+                            }
+                        </p>
+
+                        <div className="row">
+
+                            <div className="columns contact-details">
+
+                                <h2>Contact Details</h2>
+                                <p className="address">
+                                    <span>{resumeData.name}</span>
+                                    <br></br>
+                                    <span>
+                     {resumeData.address}
+                    </span>
+                                    <br></br>
+                                    <span>{resumeData.website}</span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        );
+    }
+}
---
+++ src/resumeData.js
@@ -0,0 +1,115 @@
+let resumeData = {
+    "imagebaseurl":"https://techonoeticsclub.github.io/",
+    "name": "Jake Luby",
+    "role": "Full Stack Developer",
+    "linkedinId":"jake-luby-135a5529",
+    "roleDescription": "I'm a full stack developer with strong front-end, object oriented, and DevOps skills.  With over 10 years of SDLC experience, I am confident I can take any idea and develop a clean, workable solution fast.",
+    "socialLinks":[
+        {
+            "name":"linkedin",
+            "url":"https://www.linkedin.com/in/jake-luby-135a5529/",
+            "className":"fa fa-linkedin"
+        },
+        {
+            "name":"github",
+            "url":"http://github.com/aceluby",
+            "className":"fa fa-github"
+        }
+    ],
+    "aboutme":"In a nutshell, I'm a technologist. Whether it be through the lens of a BA, PM, QA, Dev, or DevOps; I bring the love of technology, learning, and breadth of experience to every job. I can bring an idea to reality by asking the right business questions; fleshing out exact requirements; designing an architecture for the future; writing supportable code; automating the testing process via unit, integration, performance, and end-to-end functional testing; and leading the team so everyone is on the same page to deliver results as fast as possible. ",
+    "address":"5331 Florida Ave N Crystal MN 55428",
+    "website":"",
+    "education":[
+        {
+            "UniversityName":"Macalester College",
+            "specialization":"Computer Science & Math",
+            "MonthOfPassing":"May",
+            "YearOfPassing":"2005",
+            "Achievements":"Cum Laude, Phi Beta Kappa Officer"
+        }
+    ],
+    "work":[
+        {
+            "CompanyName":"Target",
+            "specialization":"Lead Engineer",
+            "MonthOfLeaving":"",
+            "YearOfLeaving":"Current",
+            "Achievements":"Rewrote the entire purchasing system in Java Spring, developed the CI/CD process, and wrote numerous web applications to support the global supply chain"
+        },
+        {
+            "CompanyName":"Data Recognition Corporation",
+            "specialization":"Sr. QA Engineer",
+            "MonthOfLeaving":"Dec",
+            "YearOfLeaving":"2012",
+            "Achievements":"Implemented their automation testing framework, seamlessly integrating into the development build and deployment pipeline"
+        },
+        {
+            "CompanyName":"SHPS",
+            "specialization":"Business Systems Analyst",
+            "MonthOfLeaving":"May",
+            "YearOfLeaving":"2008",
+            "Achievements":"Implemented dozens of new feeds while providing automated solutions for data entry"
+        }
+    ],
+    "skillsDescription":"",
+    "skills":[
+        {
+            "skillname":"OO Design and Analysis: Java Spring, Springboot, C#",
+            "skillProficiency": "100%"
+        },
+        {
+            "skillname":"Front End: ReactJs, AngularJs, Javascript, HTML5, CSS",
+            "skillProficiency": "80%"
+        },
+        {
+            "skillname":"DB: PostreSql, Cassandra, MongoDB, MSSQL, Sybase, MySql",
+            "skillProficiency": "85%"
+        },
+        {
+            "skillname":"CI/CD: Bash, Chef, Jenkins, Drone",
+            "skillProficiency": "95%"
+        },
+        {
+            "skillname":"Infrastructure: Kubernetes, OpenStack, Kafka, ELK",
+            "skillProficiency": "90%"
+        }
+    ],
+    "portfolio":[
+        {
+            "name":"Integrated Asset Network",
+            "description":"ReactJs App",
+            "imgurl":"images/portfolio/phone.jpg"
+        },
+        {
+            "name":"Luby Studios",
+            "description":"ReactJs App",
+            "imgurl":"images/portfolio/project.jpg"
+        },
+        {
+            "name":"New Hope Bowl",
+            "description":"AngularJs App",
+            "imgurl":"images/portfolio/project2.png"
+        },
+        {
+            "name":"Talent On Tap",
+            "description":"ReactJs App",
+            "imgurl":"images/portfolio/project2.png"
+        }
+    ],
+    "testimonials":[
+        {
+            "description":"Jake is a rockstar!  While working together he was consistently the guy to roll up his sleeves and get things done.",
+            "name":"Dan Gill - Director Optum Health"
+        },
+        {
+            "description":"Jake consistently over delivers and steps into any role asked.  His team is consistently one of my top performing teams and I accredit a lot of that to his leadership.",
+            "name":"Kraig Narr - VP Target"
+        },
+        {
+            "description":"Jake makes my job easy.  He's always asking the right business questions to deliver the right product.  His team is a well oiled agile machine, simplifying the process so we can focus on what should be focused on.",
+            "name":"Matthew Anderson - Senior Engineering Manager Target"
+        }
+    ]
+}
+
+export default resumeData
